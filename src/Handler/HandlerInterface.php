f217a98c72c013fc51759f13d92ef1794cc40cb5
jekuaitk <jekua@aarhus.dk>
2021-08-10 09:45:07 +0200

    Added description of handler functions

--- src/Handler/HandlerInterface.php
+++ src/Handler/HandlerInterface.php
@@ -5,17 +5,19 @@ namespace ItkDev\Adgangsstyring\Handler;
 interface HandlerInterface
 {
     /**
-     *
+     * Collect users and create deletion list.
      */
     public function collectUsersForDeletionList(): void;
 
     /**
+     * Remove users provided from the deletion list.
+     *
      * @param array $users
      */
     public function removeUsersFromDeletionList(array $users): void;
 
     /**
-     *
+     * Handle user deletion list, as no more users will be removed.
      */
     public function commitDeletionList(): void;
 }
