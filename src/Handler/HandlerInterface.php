d73eacfd72fcc56b9cc77774751663b59b34e27b
jekuaitk <jekua@aarhus.dk>
2021-08-13 14:27:23 +0200

    Added Docblock to HandlerInterface

--- src/Handler/HandlerInterface.php
+++ src/Handler/HandlerInterface.php
@@ -2,6 +2,15 @@
 
 namespace ItkDev\Adgangsstyring\Handler;
 
+/**
+ * Interface HandlerInterface
+ *
+ * Interface allowing users to implement how to handle
+ * create a user deletion list, how to remove users from and
+ * how to handle the list upon finishing the Azure AD Delta Sync flow.
+ *
+ * @package ItkDev\Adgangsstyring\Handler
+ */
 interface HandlerInterface
 {
     /**
